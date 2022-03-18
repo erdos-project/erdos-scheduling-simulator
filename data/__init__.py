9494efcb16319c66c5816fa4f063ce293a9b540d
Sukrit Kalra <sukritkalra@gmail.com>
2022-03-18 13:40:14 -0700

    Adding Plotting utils and IPython Notebook.

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,9 @@
 # Export the loaders.
 from .task_loader import TaskLoader
 from .worker_loader import WorkerLoader
+
+# Export the plotter.
+from .plotter import Plotter, SimulatorStart, SimulatorEnd,\
+    TaskRelease, TaskFinished, MissedDeadline, SchedulerStart,\
+    SchedulerFinished, Task, WorkerPool, WorkerPoolStats,\
+    Scheduler, TaskPlacement, WorkerPoolUtilization
