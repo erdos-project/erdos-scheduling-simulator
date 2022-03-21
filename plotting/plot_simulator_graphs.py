5702f62d9310a6e299a58a9ee35e98cee2702775
Ionel Gog <gogionel@gmail.com>
2022-03-21 13:06:51 -0700

    Added a plotting script for simulation experiments.

---
+++ plotting/plot_simulator_graphs.py
@@ -0,0 +1,215 @@
+from absl import app, flags
+
+from operator import attrgetter
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.patches import Patch
+
+from data import *
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('csv_file', '../test.csv',
+                    'CSV file containing experiment logs')
+flags.DEFINE_bool('plot_scheduler_info', True, 'Plot scheduling runtime')
+flags.DEFINE_bool('plot_utilization', True, 'Plot resource utilization')
+flags.DEFINE_bool('plot_task_info', True, 'Plot task placement info')
+flags.DEFINE_string('utilization_timeline_plot_name',
+                    'utilization_bar_chart.png',
+                    'The filename of the utilization timeline plot')
+flags.DEFINE_string('scheduler_runtime_timeline_plot_name',
+                    'scheduler_runtime_timeline.png',
+                    'The filename of the scheduler runtime timeline plot')
+flags.DEFINE_string('scheduler_runtime_cdf_plot_name',
+                    'scheduler_runtime_cdf.png',
+                    'The filename of the scheduler runtime CDF plot')
+flags.DEFINE_string('task_placement_bar_chart_plot_name',
+                    'tasks_placed_bar_chart.png',
+                    'The filename of the task placement bar char plot')
+
+matplotlib.rcParams.update({'font.size': 16, 'figure.autolayout': True})
+matplotlib.rcParams['xtick.labelsize'] = 26
+matplotlib.rcParams['ytick.labelsize'] = 26
+plt.rcParams['font.family'] = 'serif'
+axes_fontsize = 32
+
+
+def plot_utilization(plotter, figure_size=(14, 10), bar_width=0.30):
+    # Plotting defaults.
+    hatches = ['//', '--', '**']
+    alphas = np.arange(0.2, 1.2, 0.2)
+
+    # Worker Pool statistics
+    worker_pool_stats = plotter.get_worker_pool_utilizations(FLAGS.csv_file)
+
+    # Find all the resource types in the system.
+    resource_types = set()
+    for stats in worker_pool_stats:
+        for resource in stats.resource_utilizations.keys():
+            resource_types.add(resource)
+
+    # Calculate the heights of utilization for each resource, after normalization.
+    resource_used_heights = {
+        resource: [
+            stat.resource_utilizations[resource][0] /
+            sum(stat.resource_utilizations[resource])
+            for stat in worker_pool_stats
+        ]
+        for resource in resource_types
+    }
+    resource_free_heights = {
+        resource: [
+            stat.resource_utilizations[resource][1] /
+            sum(stat.resource_utilizations[resource])
+            for stat in worker_pool_stats
+        ]
+        for resource in resource_types
+    }
+    print(max(resource_used_heights['CPU']))
+    print(max(resource_used_heights['GPU']))
+
+    # Plot a histogram with the results.
+    fig = plt.figure(figsize=figure_size)
+    x_vals = np.arange(1, len(worker_pool_stats) + 1)
+    resource_used_bars = [
+        plt.bar(
+            x_vals + bar_width * i,
+            resource_used_heights[resource_type],
+            width=bar_width,
+            #edgecolor='black',
+            #hatch=hatches[i],
+            alpha=0.4,
+            color='red',
+        ) for i, resource_type in enumerate(resource_types)
+    ]
+
+    plt.xlabel('Worker pool utilization at each scheduler invocation',
+               fontsize=axes_fontsize)
+    plt.ylabel('Normalized utilization', fontsize=axes_fontsize)
+    # Set the y limits so we can visualize the legend.
+    ax = fig.gca()
+    ax.set_ylim(0, 1.01)
+    plt.yticks([0, 0.5, 1.0])
+    plt.savefig(FLAGS.utilization_timeline_plot_name, bbox_inches='tight')
+
+
+def plot_scheduler_info(plotter, figure_size=(14, 10)):
+    # Retrieve the runtime of the scheduler invocations.
+    scheduler_invocations = plotter.get_scheduler_invocations(FLAGS.csv_file)
+    runtimes = list(map(attrgetter('runtime'), scheduler_invocations))
+    start_times = list(map(attrgetter('start_time'), scheduler_invocations))
+    # Plot a timelapse of the runtime of the scheduler.
+    fig = plt.figure(figsize=figure_size)
+    plt.plot(runtimes, marker='o')
+    plt.xlabel('Timeline', fontsize=axes_fontsize)
+    plt.ylabel('Runtime [ms]', fontsize=axes_fontsize)
+    plt.savefig(FLAGS.scheduler_runtime_timeline_plot_name,
+                bbox_inches='tight')
+
+    # Plot the CDF of the runtime of the scheduler invocations.
+    count, bin_count = np.histogram(runtimes, bins=100)
+    pdf = count / sum(count)
+    cdf = np.cumsum(pdf)
+
+    fig = plt.figure(figsize=figure_size)
+    plt.plot(bin_count[1:], cdf, label='CDF')
+    plt.xlabel('Runtime [ms]', fontsize=axes_fontsize)
+    plt.ylabel('CDF', fontsize=axes_fontsize)
+    plt.legend(prop={'size': 28})
+    plt.savefig(FLAGS.scheduler_runtime_cdf_plot_name, bbox_inches='tight')
+
+
+def plot_task_info(plotter, figure_size=(14, 10)):
+    scheduler_invocations = plotter.get_scheduler_invocations(FLAGS.csv_file)
+    # Calculate the heights of placed and unplaced tasks.
+    placed_task_heights = [
+        scheduler_invocation.placed_tasks
+        for scheduler_invocation in scheduler_invocations
+    ]
+    unplaced_task_heights = [
+        scheduler_invocation.unplaced_tasks
+        for scheduler_invocation in scheduler_invocations
+    ]
+
+    # Plot a histogram with the results.
+    fig = plt.figure(figsize=figure_size)
+    x_vals = np.arange(1, len(scheduler_invocations) + 1)
+
+    placed_task_bar = plt.bar(
+        x_vals,
+        placed_task_heights,
+        #edgecolor='black',
+        #hatch='//',
+        alpha=0.4,
+        color='green')
+
+    unplaced_task_bar = plt.bar(
+        x_vals,
+        unplaced_task_heights,
+        bottom=placed_task_heights,
+        #edgecolor='black',
+        #hatch='**',
+        alpha=0.4,
+        color='red')
+
+    # Add the axis labels.
+    plt.xlabel('Timeline', fontsize=axes_fontsize)
+    plt.ylabel('Placed and Unplaced Tasks', fontsize=axes_fontsize)
+
+    # Add the yticks
+    max_y_val = max(scheduler_invocation.total_tasks
+                    for scheduler_invocation in scheduler_invocations)
+    y_ticks = list(range(int(max_y_val)))
+    plt.yticks(y_ticks)
+
+    # Set the legend.
+    legend_elements = [
+        Patch(facecolor='green', hatch='//', label='Placed Tasks'),
+        Patch(facecolor='red', hatch='**', label='Unplaced Tasks')
+    ]
+    plt.legend(handles=legend_elements, prop={'size': 28}, framealpha=0)
+    plt.savefig(FLAGS.task_placement_bar_chart_plot_name, bbox_inches='tight')
+
+    # Plot a histogram of the slack from the deadline for the tasks.
+    tasks = plotter.get_tasks(FLAGS.csv_file)
+
+    slack = [task.deadline - task.completion_time for task in tasks]
+    plt.hist(slack, density=True, bins=10)
+
+
+def task_stats(tasks):
+    for task in tasks:
+        print("Task Name: {}\n\tRelease Time: {}\n\t".format(
+            task.task_name, task.release_time))
+        print("Completion Time: {}\n\tDeadline: {}\n\t".format(
+            task.completion_time, task.deadline))
+        print("Runtime: {}\n\tSlack: {}".format(
+            task.runtime, task.deadline - task.completion_time))
+
+
+def main(argv):
+    figure_size = (14, 10)
+    # Load the events from the CSV file into the Plotter class.
+    plotter = Plotter(csv_paths=[FLAGS.csv_file])
+    events = plotter._events[FLAGS.csv_file]
+
+    print('Simulation end time: {}'.format(
+        plotter.get_simulator_end_time(FLAGS.csv_file)))
+    print('Number of tasks: {}'.format(len(plotter.get_tasks(FLAGS.csv_file))))
+
+    # Missed deadline statistics
+    missed_deadline_events = plotter.get_missed_deadline_events(FLAGS.csv_file)
+    print('Number of missed deadlines: {}'.format(len(missed_deadline_events)))
+
+    if FLAGS.plot_utilization:
+        plot_utilization(plotter, figure_size)
+    if FLAGS.plot_scheduler_info:
+        plot_scheduler_info(plotter, figure_size)
+    if FLAGS.plot_task_info:
+        plot_task_info(plotter, figure_size)
+
+
+if __name__ == '__main__':
+    app.run(main)
