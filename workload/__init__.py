58f19704890202812b599208135ce9563fc75797
Sukrit Kalra <sukritkalra@gmail.com>
2022-05-23 11:34:33 -0700

    Abstract Resource and Resources into separate files.

--- workload/__init__.py
+++ workload/__init__.py
@@ -1,4 +1,5 @@
 # Expose the abstractions of the workload from the module.
 from .jobs import Job, JobGraph
-from .resources import Resource, Resources
+from .resource import Resource
+from .resources import Resources
 from .tasks import Task, TaskGraph, TaskState
