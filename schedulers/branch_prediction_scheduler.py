0f57988b70489ad29fc701b041500b2ab35feecd
Sukrit Kalra <sukritkalra@gmail.com>
2022-08-30 12:40:49 -0700

    Implement prediction policies in BranchPredictionScheduler.

--- schedulers/branch_prediction_scheduler.py
+++ schedulers/branch_prediction_scheduler.py
@@ -1,3 +1,6 @@
+import random
+from collections import defaultdict
+from enum import Enum
 from typing import Optional, Sequence, Tuple
 
 import absl  # noqa: F401
@@ -6,6 +9,7 @@ from schedulers import BaseScheduler
 from utils import EventTime
 from workers import WorkerPools
 from workload import Task, Workload
+from workload.tasks import TaskGraph
 
 
 class BranchPredictionScheduler(BaseScheduler):
@@ -18,8 +22,16 @@ class BranchPredictionScheduler(BaseScheduler):
             the scheduler returns the actual runtime.
     """
 
+    class Policy(Enum):
+        """Represents the different prediction policies used by the Scheduler."""
+
+        WORST_CASE = 1  # Always predict the branch that will not be taken.
+        BEST_CASE = 2  # Always predict the branch that will be taken.
+        RANDOM = 3  # Randomly choose a branch to be taken.
+
     def __init__(
         self,
+        policy: "Policy" = Policy.RANDOM,
         preemptive: bool = False,
         runtime: EventTime = EventTime(-1, EventTime.Unit.US),
         _flags: Optional["absl.flags"] = None,
@@ -27,8 +39,79 @@ class BranchPredictionScheduler(BaseScheduler):
         super(BranchPredictionScheduler, self).__init__(
             preemptive=preemptive, runtime=runtime, _flags=_flags
         )
+        self._policy = policy
 
     def schedule(
         self, sim_time: EventTime, workload: Workload, worker_pools: WorkerPools
     ) -> Tuple[EventTime, Sequence[Tuple[Task, str, EventTime]]]:
         pass
+
+    def compute_remaining_time(self, task_graph: TaskGraph) -> EventTime:
+        """Computes the slack of the given `TaskGraph` at the `sim_time` using
+        the prediction policy specified in the Scheduler.
+
+        Args:
+            sim_time: The time at which to compute the slack.
+            task_graph: The graph for which to compute the slack.
+
+        Returns:
+            The slack as per the specified branch prediction policy.
+        """
+        remaining_time = defaultdict(lambda: EventTime.zero())
+        # Add the remaining time for the sources.
+        for source_task in task_graph.get_source_tasks():
+            remaining_time[source_task] = source_task.remaining_time
+
+        # Iterate over the other nodes and choose the remaining
+        # time according to the policy.
+        for task in task_graph.topological_sort():
+            # The branch associated with this task was not executed, skipping.
+            if task not in remaining_time:
+                continue
+
+            if task.conditional:
+                children_tasks = task_graph.get_children(task)
+                # If the task is conditional, propagate the remaining time
+                # to the children according to the policy in the scheduler.
+                if self.policy == self.Policy.WORST_CASE:
+                    # Choose the branch that has the lowest probability.
+                    child_to_release = children_tasks[0]
+                    for child in children_tasks[1:]:
+                        if child.probability < child_to_release.probability:
+                            child_to_release = child
+                elif self.policy == self.Policy.BEST_CASE:
+                    # Choose the branch that has the highest probability.
+                    child_to_release = children_tasks[0]
+                    for child in children_tasks[1:]:
+                        if child.probability > child_to_release.probability:
+                            child_to_release = child
+                elif self.policy == self.Policy.RANDOM:
+                    # Choose a branch randomly.
+                    child_to_release = random.choice(children_tasks)
+                else:
+                    raise NotImplementedError(
+                        f"The policy {self.policy} is not implemented yet."
+                    )
+
+                # Propagate the remaining time to the child.
+                remaining_time[child_to_release] = (
+                    remaining_time[task] + child_to_release.remaining_time
+                )
+            else:
+                # If the task is not conditional, propogate the remaining time
+                # to all the children.
+                for child_task in task_graph.get_children(task):
+                    if (
+                        remaining_time[child_task]
+                        <= remaining_time[task] + child_task.remaining_time
+                    ):
+                        remaining_time[child_task] = (
+                            remaining_time[task] + child_task.remaining_time
+                        )
+
+        # Find the maximum remaining time across all the sink nodes.
+        return max([remaining_time[sink] for sink in task_graph.get_sink_tasks()])
+
+    @property
+    def policy(self):
+        return self._policy
--- tests/test_schedulers.py
+++ tests/test_schedulers.py
@@ -1,10 +1,16 @@
 from logging import raiseExceptions
 
-from schedulers import EDFScheduler, GurobiScheduler, LSFScheduler, Z3Scheduler
+from schedulers import (
+    BranchPredictionScheduler,
+    EDFScheduler,
+    GurobiScheduler,
+    LSFScheduler,
+)
 from tests.utils import create_default_task
 from utils import EventTime
 from workers import Worker, WorkerPool, WorkerPools
 from workload import Resource, Resources, TaskGraph
+from workload.jobs import Job
 from workload.workload import Workload
 
 
@@ -422,3 +428,82 @@ def test_lsf_scheduler_limited_resources():
     assert (
         placements[1][1] is None
     ), "Incorrect placement of the task on the WorkerPool."
+
+
+def test_branch_prediction_scheduler_slack():
+    """Test that the BranchPredictionScheduler calculates the slack
+    correctly in case of conditional execution of nodes."""
+    # Create the tasks and the TaskGraph.
+    perception_task = create_default_task(
+        name="Perception@0",
+        job=Job(
+            name="Perception",
+            runtime=EventTime(1000, EventTime.Unit.US),
+            conditional=True,
+        ),
+        runtime=1000,
+    )
+    prediction_task_0 = create_default_task(
+        name="Prediction@0",
+        job=Job(
+            name="Prediction_0",
+            runtime=EventTime(2000, EventTime.Unit.US),
+            probability=1.0,
+        ),
+        runtime=2000,
+    )
+    prediction_task_1 = create_default_task(
+        name="Prediction@1",
+        job=Job(name="Prediction_1", runtime=EventTime(4000, EventTime.Unit.US)),
+        runtime=4000,
+    )
+    planning_task_0 = create_default_task(
+        name="Planning@0",
+        job=Job(
+            name="Planning_0",
+            runtime=EventTime(6000, EventTime.Unit.US),
+            probability=0.0,
+        ),
+        runtime=6000,
+    )
+    planning_task_1 = create_default_task(
+        name="Planning@1",
+        job=Job(name="Planning_1", runtime=EventTime(8000, EventTime.Unit.US)),
+        runtime=8000,
+    )
+    perception_terminal_task = create_default_task(
+        name="Perception_End@0",
+        job=Job(name="Perception_End", runtime=EventTime.zero()),
+        runtime=0,
+    )
+    task_graph = TaskGraph(
+        {
+            perception_task: [prediction_task_0, planning_task_0],
+            prediction_task_0: [prediction_task_1],
+            planning_task_0: [planning_task_1],
+            prediction_task_1: [perception_terminal_task],
+            planning_task_1: [perception_terminal_task],
+            perception_terminal_task: [],
+        }
+    )
+
+    # Assert schedulers with different policies and remaining time.
+    worst_case_scheduler = BranchPredictionScheduler(
+        policy=BranchPredictionScheduler.Policy.WORST_CASE
+    )
+    slack = worst_case_scheduler.compute_remaining_time(task_graph)
+    assert slack == EventTime(15, EventTime.Unit.MS), "Incorrect slack returned."
+
+    best_case_scheduler = BranchPredictionScheduler(
+        policy=BranchPredictionScheduler.Policy.BEST_CASE
+    )
+    slack = best_case_scheduler.compute_remaining_time(task_graph)
+    assert slack == EventTime(7, EventTime.Unit.MS), "Incorrect slack returned."
+
+    random_scheduler = BranchPredictionScheduler(
+        policy=BranchPredictionScheduler.Policy.RANDOM
+    )
+    slack = random_scheduler.compute_remaining_time(task_graph)
+    assert slack == EventTime(7, EventTime.Unit.MS) or slack == EventTime(
+        15, EventTime.Unit.MS
+    ), "Incorrect slack returned."
