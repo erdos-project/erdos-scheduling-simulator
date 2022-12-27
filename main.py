f22ba7596661501c9073b0964965de6185152b05
Sukrit Kalra <sukritkalra@gmail.com>
2022-12-26 22:58:04 -0800

    [BUG] Fix flags validator for branch_prediction_accuracy.

--- main.py
+++ main.py
@@ -198,7 +198,7 @@ flags.DEFINE_float(
 flags.register_multi_flags_validator(
     ["branch_prediction_accuracy", "resolve_conditionals_at_submission"],
     lambda values: not values["resolve_conditionals_at_submission"]
-    or values["branch_prediction_accuracy"] == 0.50,
+    or values["branch_prediction_accuracy"] != 0.50,
     message="branch_prediction_accuracy can only be set when "
     "conditionals are resolved at submission time.",
 )
