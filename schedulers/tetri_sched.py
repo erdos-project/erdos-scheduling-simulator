5acedb3286ed4413edf158b7dd6c9c2623cd627a
Peter Schafhalter <pschafhalter@berkeley.edu>
2023-03-21 03:07:25 -0700

    Formatting

--- schedulers/tetri_sched.py
+++ schedulers/tetri_sched.py
@@ -9,11 +9,11 @@ from typing import Mapping, Optional, Sequence, Tuple, Union
 import absl  # noqa: F401
 import gurobipy as gp
 from gurobipy import GRB
+
+from schedulers import BaseScheduler
 from utils import EventTime
 from workers import Worker, WorkerPools
 from workload import Placement, Placements, Task, TaskState, Workload
-
-from schedulers import BaseScheduler
 
 
 class SpaceTimeMatrix:
