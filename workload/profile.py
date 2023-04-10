f8c3953a7bc9482e0a450aaf2df0eb8e3aaf21c2
Sukrit Kalra <sukritkalra@gmail.com>
2023-04-09 22:51:30 -0700

    Add the loading strategies to the WorkProfile.

--- workload/profile.py
+++ workload/profile.py
@@ -10,13 +10,21 @@ class WorkProfile(object):
 
     Args:
         name (`str`): A name for this profile.
+        loading_strategies (`ExecutionStrategies`): The strategies used for loading this
+            profile onto a `Worker`.
         execution_strategies (`ExecutionStrategies`): The list of `ExecutionStrategies`
             that can be used to execute the task.
     """
 
-    def __init__(self, name: str, execution_strategies: ExecutionStrategies) -> None:
+    def __init__(
+        self,
+        name: str,
+        execution_strategies: ExecutionStrategies,
+        loading_strategies: ExecutionStrategies = ExecutionStrategies(),
+    ) -> None:
         self._name = name
         self._id = uuid.uuid4()
+        self._loading_strategies = loading_strategies
         self._execution_strategies = execution_strategies
 
     def __eq__(self, __value: object) -> bool:
@@ -25,6 +33,10 @@ class WorkProfile(object):
     @property
     def name(self) -> str:
         return self._name
+
+    @property
+    def loading_strategy(self) -> ExecutionStrategies:
+        return self._loading_strategies
 
     @property
     def execution_strategies(self) -> ExecutionStrategies:
