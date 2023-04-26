8d084f9cdbdea6cb9a217371310c0ee69e809920
Peter Schafhalter <pschafhalter@berkeley.edu>
2023-04-25 23:44:28 -0700

    Add script to reproduce Clockwork Fig 5 results

---
+++ scripts/run_clockwork_microbenchmark.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+scheduler=${1:-Clockwork}
+
+SLOS=(    10  25  50  100 250 500)
+GOODPUTS=(400 500 575 675 725 800)
+
+for i in ${!SLOS[@]}
+do
+  slo=${SLOS[$i]}
+  goodput=${GOODPUTS[$i]}
+  echo "Running Clockwork microbenchmark with $scheduler, SLO $slo, goodput $goodput"
+  request_rate=$(bc -l <<< "$goodput / (15 * 1000000)")
+  filename="clockwork-microbenchmark-$scheduler-slo-$slo-goodput-$goodput"
+  python3 main.py \
+      --flagfile=configs/clockwork_resnet50_microbenchmark.conf \
+      --override_poisson_arrival_rate=$request_rate \
+      --override_slo=$slo \
+      --log="$filename.log" \
+      --csv="$filename.csv"
+done
