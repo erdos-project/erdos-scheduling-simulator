d6bebe6de4c39bcc0dae0d7aff82b1c7652d61f4
Sukrit Kalra <sukritkalra@gmail.com>
2023-08-30 11:49:25 -0700

    Try-importing the CPP modules.

--- schedulers/erdos_scheduler.py
+++ schedulers/erdos_scheduler.py
@@ -1,4 +1,8 @@
-from erdos_scheduler_cpp import ERDOSScheduler as ERDOSchedulerCPP
+try:
+    from erdos_scheduler_cpp import ERDOSScheduler as ERDOSchedulerCPP
+except ModuleNotFoundError:
+    print("ERDOSSchedulerCPP was not found. Please compile the C++ implementation.")
+
 
 from schedulers import BaseScheduler
 from utils import EventTime
