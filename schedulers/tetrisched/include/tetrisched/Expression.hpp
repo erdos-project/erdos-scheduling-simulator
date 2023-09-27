d59894ecc5f44a8a71d625b87e7e6a281aef1c7f
Alind Khare <alindkhare@gatech.edu>
2023-09-26 21:03:16 -0400

    Added extraneous variable

--- schedulers/tetrisched/include/tetrisched/Expression.hpp
+++ schedulers/tetrisched/include/tetrisched/Expression.hpp
@@ -156,9 +156,6 @@ protected:
     ObjectiveType objectiveType;
     /// The children of this Expression.
     std::vector<ExpressionPtr> children;
-    int minUvaridx;
-    int minUStartTime;
-    int minEndTime;
 
 public:
     
