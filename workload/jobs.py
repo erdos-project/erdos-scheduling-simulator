fcb1ee679fe337398033a917e4d8ed6efdcb4c80
Alind Khare <asaditya1195@gmail.com>
2023-11-29 22:44:11 -0500

    Round instead of clip in gamma release time generation

--- workload/jobs.py
+++ workload/jobs.py
@@ -301,12 +301,12 @@ class JobGraph(Graph[Job]):
                     )
                 current_release_time = self._start.time
                 releases.append(EventTime(
-                        int(current_release_time), EventTime.Unit.US
+                        round(current_release_time), EventTime.Unit.US
                     ))
                 for inter_arrival_time in inter_arrival_times:
                     current_release_time += inter_arrival_time 
                     releases.append(EventTime(
-                        int(current_release_time), EventTime.Unit.US
+                        round(current_release_time), EventTime.Unit.US
                     ))
             elif self._policy_type == JobGraph.ReleasePolicyType.CLOSED_LOOP:
                 # Release the first set of Tasks at the start time.
