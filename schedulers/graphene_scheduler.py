4e8beff2abb8f8218c1f7108146c7374c93bbf3b
Sukrit Kalra <sukritkalra@gmail.com>
2024-02-22 13:20:54 -0800

    Finish the implementation of the offline pass of Graphene.

--- schedulers/graphene_scheduler.py
+++ schedulers/graphene_scheduler.py
@@ -1,5 +1,8 @@
 import os
-from typing import Optional
+from collections import defaultdict
+from itertools import groupby
+from operator import attrgetter
+from typing import List, Mapping, Optional, Sequence
 
 import absl  # noqa: F401
 import numpy as np
@@ -7,7 +10,7 @@ import tetrisched_py as tetrisched
 
 from utils import EventTime
 from workers import WorkerPools
-from workload import Placements, Workload
+from workload import Placements, Task, TaskGraph, Workload
 
 from .tetrisched_scheduler import Partitions, TetriSchedScheduler
 
@@ -60,6 +63,7 @@ class GrapheneScheduler(TetriSchedScheduler):
             log_to_file=log_to_file,
             _flags=_flags,
         )
+        self._log_graphs = _flags.log_graphs if _flags else False
 
         # The Graphene scheduler is a STRL-based scheduler, and it requires the
         # TetriSched framework to solve the STRL formulation.
@@ -178,9 +182,145 @@ class GrapheneScheduler(TetriSchedScheduler):
                 self._min_makespan_scheduler.exportLastSolverModel(
                     os.path.join(self._log_dir, solver_model_file_name)
                 )
+                raise e
 
-                # TODO (Sukrit): Retrieve the order of placements from the solver, and
-                # construct the new TaskGraph with the placements.
+            # TODO (Sukrit): Retrieve the order of placements from the solver, and
+            # construct the new TaskGraph with the placements.
+            if not self._min_makespan_scheduler.getLastSolverSolution().isValid():
+                strl_file_name = f"{task_graph_name}_error.dot"
+                solver_model_file_name = f"{task_graph_name}_error.lp"
+                self._logger.error(
+                    "[%s] The minimum makespan scheduler failed to find a solution "
+                    "for the STRL expression of the TaskGraph %s. Dumping the model "
+                    "to %s and the STRL expression to %s.",
+                    sim_time.time,
+                    task_graph_name,
+                    solver_model_file_name,
+                    strl_file_name,
+                )
+                raise ValueError(
+                    f"Failed to find a minimum makespan solution "
+                    f"for the TaskGraph {task_graph_name}."
+                )
+
+            # Retrieve the solution and check if we were able to find a valid solution.
+            task_graph_solution = objective_strl.getSolution()
+            if task_graph_solution is not None and task_graph_solution.utility > 0:
+                self._logger.info(
+                    "[%s] Successfully found a minimum makespan solution for "
+                    "TaskGraph %s and the utility was %s.",
+                    sim_time.time,
+                    task_graph_name,
+                    task_graph_solution.utility,
+                )
+                # Retrieve all the Placements for the Tasks in the TaskGraph and sort
+                # them by their placement time.
+                placements = []
+                for task in task_graph.get_nodes():
+                    task_placement = task_graph_solution.getPlacement(task.unique_name)
+                    if task_placement is None:
+                        self._logger.error(
+                            "[%s] A utility for the TaskGraph %s was found, but "
+                            "the Task %s has no recorded Placement object.",
+                            sim_time.time,
+                            task.task_graph,
+                            task.unique_name,
+                        )
+                        raise ValueError(
+                            f"Failed to find a placement for the Task "
+                            f"{task.unique_name} in the TaskGraph {task.task_graph}."
+                        )
+                    placements.append(task_placement)
+
+                # Sort the placements by their start time and divide them up into stages
+                # by grouped start times. We enforce direct dependencies between all
+                # the tasks in each stage. Within a stage, we introduce dependencies
+                # between tasks that run sequentially.
+                stages = groupby(
+                    sorted(placements, key=attrgetter("startTime")),
+                    key=attrgetter("startTime"),
+                )
+                stage_to_task_map: Mapping[int, List[tetrisched.strl.Placement]] = {}
+                for index, stage in enumerate(stages):
+                    stage_start, placements = stage
+                    stage_to_task_map[index] = list(placements)
+                    self._logger.debug(
+                        "[%s] Constructed stage %s for %s with "
+                        "start time %s and %s elements.",
+                        sim_time.time,
+                        index,
+                        task_graph_name,
+                        stage_start,
+                        len(stage_to_task_map[index]),
+                    )
+
+                # Now that the stages have been constructed, we can construct an
+                # adjacency matrix for the TaskGraph by adding a dependency between
+                # a Task in stage i and all the Tasks in stage i - 1 that finish before.
+                updated_tasks_adjacency_matrix: Mapping[Task, Sequence[Task]] = (
+                    defaultdict(list)
+                )
+                for stage_index, stage in stage_to_task_map.items():
+                    for placement in stage:
+                        task = task_graph.get_task(placement.name, unique=True)
+                        if task is None:
+                            raise ValueError(
+                                f"Failed to find the Task {placement.name} "
+                                f"in TaskGraph {task_graph.name}."
+                            )
+                        for previous_placement in stage_to_task_map.get(
+                            stage_index - 1, []
+                        ):
+                            previous_task = task_graph.get_task(
+                                previous_placement.name, unique=True
+                            )
+                            if previous_task is None:
+                                raise ValueError(
+                                    f"Failed to find the Task {previous_placement.name} "
+                                    f"in TaskGraph {task_graph.name}."
+                                )
+                            strategies = previous_task.available_execution_strategies
+                            fastest_strategy = strategies.get_fastest_strategy()
+                            task_runtime = fastest_strategy.runtime.time
+                            if (
+                                previous_placement.startTime + task_runtime
+                                <= placement.startTime
+                            ):
+                                # This Task finishes before the current Task starts in
+                                # the offline order, so we add a dependency between
+                                # the two Tasks here.
+                                updated_tasks_adjacency_matrix[previous_task].append(
+                                    task
+                                )
+
+                # Now that the adjacency matrix has been constructed, we construct a
+                # new TaskGraph with the updated adjacency matrix, and swap it with the
+                # adjacency matrix in the original TaskGraph.
+                if self._log_graphs:
+                    # Dump the original TaskGraph to the log directory.
+                    task_graph.to_dot(
+                        os.path.join(self._log_dir, f"{task_graph_name}_original.dot")
+                    )
+                task_graph.update_edges(updated_tasks_adjacency_matrix)
+                if self._log_graphs:
+                    # Dump the updated TaskGraph to the log directory.
+                    task_graph.to_dot(
+                        os.path.join(self._log_dir, f"{task_graph_name}_updated.dot")
+                    )
+            else:
+                self._logger.error(
+                    "[%s] Failed to find a minimum makespan solution for TaskGraph %s.",
+                    sim_time.time,
+                    task_graph_name,
+                )
+                raise ValueError(
+                    f"Failed to find a minimum makespan solution "
+                    f"for the TaskGraph {task_graph_name}."
+                )
+
+        raise NotImplementedError(
+            "Saving of the transformed TaskGraphs is not yet done."
+        )
 
         # All the TaskGraphs have been transformed, call the TetriSched scheduler and
         # return the Placements.
