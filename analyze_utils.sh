1faa1ce06749fcb75a065fc420399706963f36c9
Sukrit Kalra <sukritkalra@gmail.com>
2024-03-19 15:57:34 -0700

    Add helper bash functions.

---
+++ analyze_utils.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# cancelled() method takes a CSV as input and outputs the number of
+# TaskGraphs that have been cancelled.
+cancelled() {
+    if [[ "$1" == *.csv ]]; then
+        cat $1 | grep "TASK_CANCEL" | awk -F, "{print $6}" | sort | uniq | wc -l
+    else
+	echo "cancelled() takes a CSV file as input."
+    fi
+}
