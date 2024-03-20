63bf3a2f8fc256ff9c13efbedfb57772c65839aa
Sukrit Kalra <sukritkalra@gmail.com>
2024-03-20 16:41:07 -0700

    Fix script for bash utilities.

--- analyze_utils.sh
+++ analyze_utils.sh
@@ -4,7 +4,7 @@
 # TaskGraphs that have been cancelled.
 cancelled() {
     if [[ "$1" == *.csv ]]; then
-        cat $1 | grep "TASK_CANCEL" | awk -F, "{print $6}" | sort | uniq | wc -l
+        cat $1 | grep "TASK_CANCEL" | awk -F, '{print $6}' | sort | uniq | wc -l
     else
 	echo "cancelled() takes a CSV file as input."
     fi
