f5779cf3acd567e7f80e5e31a451f92dda13f51f
Sukrit Kalra <sukritkalra@gmail.com>
2024-03-26 15:14:08 -0700

    Add more bash functions for analysis.

--- analyze_utils.sh
+++ analyze_utils.sh
@@ -9,3 +9,23 @@ cancelled() {
 	echo "cancelled() takes a CSV file as input."
     fi
 }
+
+# released() method takes a CSV as input and outputs the number of
+# TaskGraphs that have been released.
+released() {
+    if [[ "$1" == *.csv ]]; then
+        cat $1 | grep "TASK_GRAPH_RELEASE" | wc -l
+    else
+	echo "released() takes a CSV file as input."
+    fi
+}
+
+# finished() method takes a CSV as input and outputs the number of
+# TaskGraphs that have been released.
+finished() {
+    if [[ "$1" == *.csv ]]; then
+	cat $1 | grep "TASK_GRAPH_FINISHED" | wc -l
+    else
+	echo "finished() takes a CSV file as input."
+    fi
+}
