43b833135adb0a971bc9aa777e5e38dc5ae3f5f5
Elton Leander Pinto <eltonp3103@gmail.com>
2024-11-04 17:26:06 -0500

    implement RegisterWorker

--- rpc/service.py
+++ rpc/service.py
@@ -8,7 +8,8 @@ from typing import Optional
 import main
 from schedulers import EDFScheduler
 from simulator import Simulator, EventTime
-from workers import WorkerPool, WorkerPools
+from workers import Worker, WorkerPool, WorkerPools
+from workload import Resource, Resources
 from utils import setup_logging, setup_csv_logging
 from rpc import erdos_scheduler_pb2
 from rpc import erdos_scheduler_pb2_grpc
@@ -22,6 +23,21 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer("port", 50051, "Port to serve the ERDOS Scheduling RPC Server on.")
 flags.DEFINE_integer(
     "max_workers", 10, "Maximum number of workers to use for the RPC server."
+)
+flags.DEFINE_integer(
+    "virtualized_cores",
+    500,
+    "The number of virtualized cores that must be created in each Worker on the "
+    "framework. This allows us to spawn a higher number of executors than the number "
+    "possible with actual available resources. Thus, we can spawn the executors for "
+    "each application, and only selectively activate them according to the actual "
+    "available resources.",
+)
+flags.DEFINE_integer(
+    "virtualized_memory",
+    500,
+    "The amount of virtualized memory (in GB) that must be created in each Worker on "
+    "the framework. Refer to the `virtualized_cores` flag for more information.",
 )
 
 
@@ -48,16 +64,8 @@ class Servicer(erdos_scheduler_pb2_grpc.SchedulerServiceServicer):
         self._simulator = None
         self._scheduler = EDFScheduler()
 
-    def __sim_time(self, ts: Optional[int] = None):
-        if self._initialization_time is None:
-            raise ValueErorr("initialization time is not set")
-        if not ts:
-            ts = int(time.time())
-        ts = EventTime(ts, EventTime.Unit.US)
-        return ts - self._initialization_time
-
     async def RegisterFramework(self, request, context):
-        if self._simulator:
+        if self.__framework_registered():
             msg = f"Framework already registered at the address {self._master_uri} at timestamp {self._initialization_time}"
             self._logger.error(msg)
             return erdos_scheduler_pb2.RegisterFrameworkResponse(
@@ -76,7 +84,9 @@ class Servicer(erdos_scheduler_pb2_grpc.SchedulerServiceServicer):
         )
         self._simulator = Simulator(
             scheduler=self._scheduler,
-            worker_pools=WorkerPools([worker_pool]),
+            worker_pools=WorkerPools(
+                [worker_pool]
+            ),  # Maintain only one worker pool in the simulator
         )
 
         sim_time = self.__sim_time(request.timestamp)
@@ -85,7 +95,7 @@ class Servicer(erdos_scheduler_pb2_grpc.SchedulerServiceServicer):
         return erdos_scheduler_pb2.RegisterFrameworkResponse(success=True, message=msg)
 
     async def DeregisterFramework(self, request, context):
-        if not self._simulator:
+        if not self.__framework_registered():
             msg = f"Trying to deregister a framework at {request.uri} but no framework has been registered yet."
             self._logger.error(msg)
             return erdos_scheduler_pb2.DeregisterFrameworkResponse(
@@ -122,13 +132,62 @@ class Servicer(erdos_scheduler_pb2_grpc.SchedulerServiceServicer):
         pass
 
     async def RegisterWorker(self, request, context):
-        pass
+        sim_time = self.__sim_time()
+
+        if not self.__framework_registered():
+            msg = f"[{sim_time}] Trying to register a worker (id={request.id}, name={request.name}) but no framework is registered yet"
+            return erdos_scheduler_pb2.RegisterWorkerResponse(
+                success=False, message=msg
+            )
+
+        # TODO(Sukrit): Right now, we drop the memory requirements, we should use
+        # them to do multi-dimensional packing using STRL.
+
+        cpu_resource = Resource(name="Slot_CPU")
+        worker_resources = Resources(
+            resource_vector={
+                # TODO(elton): handle override worker cpu count?
+                cpu_resource: request.cores,
+            },
+            _logger=self._logger,
+        )
+        worker = Worker(
+            name=request.id,
+            resources=worker_resources,
+            _logger=self._logger,
+        )
+
+        # Simulator maintains only one worker pool, so this should be fine
+        next(iter(self._simulator._worker_pools.worker_pools)).add_workers([worker])
+
+        msg = f"[{sim_time}] Registered worker (id={request.id}, name={request.name})."
+
+        # TODO(elton): run scheduler? i think not, but keeping a TODO here to review later
+
+        return erdos_scheduler_pb2.RegisterWorkerResponse(
+            success=True,
+            message=msg,
+            # TODO(elton): not sure why we need to set this here
+            cores=FLAGS.virtualized_cores,
+            memory=FLAGS.virtualized_memory * 1024,
+        )
 
     async def GetPlacements(self, request, context):
         pass
 
     async def NotifyTaskCompletion(self, request, context):
         pass
+
+    def __sim_time(self, ts: Optional[int] = None) -> EventTime:
+        if self._initialization_time is None:
+            raise ValueErorr("initialization time is not set")
+        if not ts:
+            ts = int(time.time())
+        ts = EventTime(ts, EventTime.Unit.US)
+        return ts - self._initialization_time
+
+    def __framework_registered(self):
+        return self._simulator is not None
 
 
 async def serve(server):
