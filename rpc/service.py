d68772dce642f664c90b7fac653d6089ff789310
Elton Leander Pinto <eltonp3103@gmail.com>
2024-11-04 15:23:16 -0500

    implement register/deregister framework

--- rpc/service.py
+++ rpc/service.py
@@ -1,6 +1,13 @@
 import asyncio
 from concurrent import futures
+from urllib.parse import urlparse
 
+# TODO: refactor out the need to import main to get common flags
+import main
+from schedulers import EDFScheduler
+from simulator import Simulator, EventTime
+from workers import WorkerPool, WorkerPools
+from utils import setup_logging, setup_csv_logging
 from rpc import erdos_scheduler_pb2
 from rpc import erdos_scheduler_pb2_grpc
 
@@ -18,13 +25,83 @@ flags.DEFINE_integer(
 
 class Servicer(erdos_scheduler_pb2_grpc.SchedulerServiceServicer):
     def __init__(self) -> None:
-        pass
+        self._logger = setup_logging(
+            name=__name__,
+            log_dir=FLAGS.log_dir,
+            log_file=FLAGS.log_file_name,
+            log_level=FLAGS.log_level,
+        )
+        self._csv_logger = setup_csv_logging(
+            name=__name__,
+            log_dir=FLAGS.log_dir,
+            log_file=FLAGS.csv_file_name,
+        )
+        for flag_name in FLAGS:
+            self._csv_logger.debug(
+                f"input_flag,{flag_name},{getattr(FLAGS, flag_name)}"
+            )
+
+        self._master_uri = None
+        self._initialization_time = None
+        self._simulator = None
+        self._scheduler = EDFScheduler()
 
     async def RegisterFramework(self, request, context):
-        pass
+        if self._simulator:
+            msg = f"Framework already registered at the address {self._master_uri} at timestamp {self._initialization_time}"
+            self._logger.error(msg)
+            return erdos_scheduler_pb2.RegisterFrameworkResponse(
+                success=False,
+                message=msg,
+            )
+
+        framework_name = request.name
+        self._master_uri = request.uri
+        self._initialization_time = EventTime(request.timestamp, EventTime.Unit.US)
+
+        parsed_uri = urlparse(self._master_uri)
+        worker_pool = WorkerPool(
+            name=f"WorkerPool_{parsed_uri.netloc}",
+            _logger=self._logger,
+        )
+        self._simulator = Simulator(
+            scheduler=self._scheduler,
+            worker_pools=WorkerPools([worker_pool]),
+        )
+
+        sim_time = (
+            EventTime(request.timestamp, EventTime.Unit.US) - self._initialization_time
+        )
+        msg = f"[{sim_time}] Registered the framework '{framework_name}' with URI {self._master_uri} at {self._initialization_time.time}"
+        self._logger.info(msg)
+        return erdos_scheduler_pb2.RegisterFrameworkResponse(success=True, message=msg)
 
     async def DeregisterFramework(self, request, context):
-        pass
+        if not self._simulator:
+            msg = f"Trying to deregister a framework at {request.uri} but no framework has been registered yet."
+            self._logger.error(msg)
+            return erdos_scheduler_pb2.DeregisterFrameworkResponse(
+                success=False, message=msg
+            )
+
+        if self._master_uri != request.uri:
+            msg = f"Trying to deregister the framework at {request.uri} but the registered framework is at {self._master_uri}"
+            self._logger.error(msg)
+            return erdos_scheduler_pb2.DeregisterFrameworkResponse(
+                success=False, message=msg
+            )
+
+        sim_time = (
+            EventTime(request.timestamp, EventTime.Unit.US) - self._initialization_time
+        )
+        self._initialization_time = None
+        self._master_uri = None
+        self._simulator = None
+        msg = f"[{sim_time}] Successfully deregistered the framework at {request.uri}"
+        self._logger.info(msg)
+        return erdos_scheduler_pb2.DeregisterFrameworkResponse(
+            success=True, message=msg
+        )
 
     async def RegisterDriver(self, request, context):
         pass
@@ -58,9 +135,7 @@ def main(_argv):
     loop = asyncio.get_event_loop()
 
     server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=FLAGS.max_workers))
-    erdos_scheduler_pb2_grpc.add_SchedulerServiceServicer_to_server(
-        Servicer(), server
-    )
+    erdos_scheduler_pb2_grpc.add_SchedulerServiceServicer_to_server(Servicer(), server)
     server.add_insecure_port(f"[::]:{FLAGS.port}")
 
     try:
