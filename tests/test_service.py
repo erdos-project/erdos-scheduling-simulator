2ec812e4c28896b7e2d3bdb751f705f29694c4e9
Dhruv Garg <dhruvsgarg@hotmail.com>
2024-11-21 13:28:36 -0500

    Add tests for notify task completion

--- tests/test_service.py
+++ tests/test_service.py
@@ -112,6 +112,58 @@ def test_service():
         )
         actual_task_ids.add(placement.task_id)
     assert actual_task_ids == {0, 1}
+    
+    # Wait for 3 seconds and trigger notify task completion for tasks 0 and 1
+    time.sleep(3)
+    
+    request = erdos_scheduler_pb2.NotifyTaskCompletionRequest(
+        application_id="task-graph-0",
+        task_id=0,
+        timestamp=1234567890
+    )
+    response = stub.NotifyTaskCompletion(request)
+    assert response.success
+        
+    request = erdos_scheduler_pb2.NotifyTaskCompletionRequest(
+        application_id="task-graph-0",
+        task_id=1,
+        timestamp=1234567890
+    )
+    response = stub.NotifyTaskCompletion(request)
+    assert response.success
+        
+    # Wait for 20s to allow the service to execute task completion for fastest task
+    time.sleep(20)
+    
+    # Attempt to incorrectly notify task completion for task 3, which hasnt started yet
+    request = erdos_scheduler_pb2.NotifyTaskCompletionRequest(
+        application_id="task-graph-0",
+        task_id=3,
+        timestamp=1234567890
+    )
+    response = stub.NotifyTaskCompletion(request)
+    assert not response.success
+    
+    # Wait 2s to allow the service to process the incorrect task completion
+    time.sleep(2)
+    
+    # Wait for 25s to allow the service to finish execution of task 0
+    time.sleep(25)
+    
+    # This will unlock task 2, which should now be returned as a placement
+    request = erdos_scheduler_pb2.GetPlacementsRequest(
+        timestamp=1234567890,
+        id="task-graph-0",
+    )
+    response = stub.GetPlacements(request)
+    assert response.success
+    actual_task_ids = set()
+    for placement in response.placements:
+        assert (
+            placement.worker_id == "1234" and placement.application_id == "task-graph-0"
+        )
+        actual_task_ids.add(placement.task_id)
+    assert actual_task_ids == {2}
 
     # Deregister framework
     request = erdos_scheduler_pb2.DeregisterFrameworkRequest(
